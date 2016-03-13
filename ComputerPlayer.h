45ccfc2f1074669942d7369bc3f2c417bbd2d0f8
Jeff McCullen <mccullen_j@cake.lynchburg.edu>
2016-03-12 19:24:37 -0500

    Add comments to ComputerPlayer.h

--- ComputerPlayer.h
+++ ComputerPlayer.h
@@ -1,7 +1,12 @@
 /**
 Name: Jeff McCullen and Emma Elliott
 Date: February 10, 2016
-Description: 
+Description: Prototype for the ComputerPlayer class. This class
+is a computer player for tic tac toe and uses a hash table to 
+store the states of the game in order to always make the best move.
+If the state of the game is not known, meaning it is not yet stored
+in the table, it is recursively figured out using the minimax
+algorithm and then stored in the table.
 */
 #ifndef COMPUTER_PLAYER_H
 #define COMPUTER_PLAYER_H
@@ -59,7 +64,24 @@ private:
 	Board::Piece compPiece_;
 	Board::Piece humanPiece_;
 
+	/**
+	Find the best move for the computer given a board.
+
+	@param board The board to find the best move for.
+	@return The best move (a structure containing the row, column, and 
+	evaluation of the move). The evaluation determines who will win
+	if the other player plays optimally or may be a draw.
+	*/
 	virtual AIMove findCompMove(Board& board);
+
+	/**
+	Find the best move for the human given a board.
+
+	@param board The board to find the best move for.
+	@return The best move (a structure containing the row, column, and 
+	evaluation of the move). The evaluation determines who will win
+	if the other player plays optimally or may be a draw.
+	*/
 	virtual AIMove findHumanMove(Board& board);
 
 	/** 
@@ -85,15 +107,37 @@ private:
 	void getMoves(Board& board, Board::Piece player,
 		std::vector<AIMove>& moves);
 
+	/**
+	Determine if the player can make a move to win the game on the
+	board. If he can, set the best move to that move. 
+
+	@param board The board to check if there is an immediate win.
+	@param player The player to check if they can win.
+	@param bestMove The move to update if there is an immediate win
+	@return True if the player can win on the board.
+	*/
 	virtual bool immediateWin(Board& board, Board::Piece player,
 		AIMove& bestMove);
 
+	/**
+	Functor which returns the hash code of the board.
+
+	@param The board to hash.
+	@return The hash code of the board.
+	*/
 	class BoardHash
 	{
 	public:
 		virtual size_t operator()(const Board& board) const;
 	};
 
+	/**
+	Functor which determines wheather two boards are equal.
+
+	@param lhs The board on the left hand side.
+	@param rhs The board on the right hand side.
+	@return True if the boards are equalivant. 
+	*/
 	class BoardEquator
 	{
 	public:
