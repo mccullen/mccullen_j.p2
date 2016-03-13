988e55f562c875c4ac79c511a3fae0b5fb48e873
Jeff McCullen <mccullen_j@cake.lynchburg.edu>
2016-03-12 19:19:56 -0500

    Add comments to ComputerPlayer.cpp

--- ComputerPlayer.cpp
+++ ComputerPlayer.cpp
@@ -1,7 +1,12 @@
 /**
 Name: Jeff McCullen and Emma Elliott
 Date: February 10, 2016
-Description: 
+Description: Implementation for the ComputerPlayer class. This class
+is a computer player for tic tac toe and uses a hash table to 
+store the states of the game in order to always make the best move.
+If the state of the game is not known, meaning it is not yet stored
+in the table, it is recursively figured out using the minimax
+algorithm and then stored in the table.
 */
 #include "Board.h"
 #include "ComputerPlayer.h"
@@ -15,6 +20,7 @@ Construct a computer player using the given piece.
 ComputerPlayer::ComputerPlayer(Board::Piece piece) : 
 	compPiece_(piece)
 {
+	// The human piece is whatever the computer piece is not.
 	humanPiece_ = (compPiece_ == Board::X) ? Board::O : Board::X;
 }
 
@@ -25,28 +31,38 @@ Make the best move on the board.
 */
 void ComputerPlayer::makeNextMove(Board& board)
 {
-	/*
-	 - If board is in table
-	   - return the score
-	 - Else
-	   - evaluate the score using minimax recursively
-	   - store it in the table.
-	*/
-
+	// Find the best move for the computer and then make that move.
 	AIMove bestMove = findCompMove(board);
 	board.playPiece(bestMove.row_, bestMove.column_, compPiece_);
 }
 
+/**
+Find the best move for the computer given a board.
+
+@param board The board to find the best move for.
+@return The best move (a structure containing the row, column, and 
+evaluation of the move). The evaluation determines who will win
+if the other player plays optimally or may be a draw.
+*/
 ComputerPlayer::AIMove ComputerPlayer::findCompMove(Board& board)
 {
+	// Variable for the bestMove the computer can make given the
+	// board.
 	AIMove bestMove;
 
+	// Variable for the evaluation of the human player assuming
+	// he plays optimally.
 	Evaluation responseValue;
 
+	// If the game is a draw, the score (evaluation) of the move is
+	// a draw.
 	if (board.gameState() == Board::DRAW)
 	{
 		bestMove.score_ = ComputerPlayer::DRAW;
 	}
+	// Else, if the computer player can make a move to win the game,
+	//  - set the bestMove row and column to that move (happens inside
+	//    immediateWin()) and set the score to COMP_WIN
 	else if (immediateWin(board, compPiece_, bestMove))
 	/*
 	else if ((board.gameState() == Board::XWINS && compPiece_ == Board::X) ||
@@ -55,38 +71,48 @@ ComputerPlayer::AIMove ComputerPlayer::findCompMove(Board& board)
 	{
 		bestMove.score_ = COMP_WIN;
 	}
+	// Else, the board has not yet reached a terminal state so
+	// figure out the bestMove by looking it up in the table or 
+	// using minimax.
 	else
 	{
+		// Set the score to assume the worst (COMP_LOSS)
 		bestMove.score_ = COMP_LOSS;
+		// Set the row and column to the first
 		bestMove.row_ = 0;
 		bestMove.column_ = 0;
+
+		// Check every square on the board
 
 		for (size_t row = 0; row < Board::MAX_ROWS; ++row)
 		{
 			for (size_t column = 0; column < Board::MAX_COLUMNS;
 				++column)
 			{
+				// If the square is empty, play there and see what the
+				// evaluation of the board is.
 				if (board.checkPiece(row, column) == Board::NONE)
 				{
 					board.playPiece(row, column, compPiece_);
-					/*
-					 - If board is in table
-					   - return the score
-					 - Else
-					   - evaluate the score using minimax recursively
-					   - store it in the table.
-					*/
+
+					// If the board is in the table, return the score
 					if (boardToEvaluation_.in(board))
 					{
 						responseValue = boardToEvaluation_[board];
 					}
+					// Else, evaluate the score using minimax recurisvely
+					// and then store it in the table.
 					else
 					{
 						responseValue = findHumanMove(board).score_;
 						boardToEvaluation_.insert(board, responseValue);
 					}
+
 					board.unplacePiece(row, column);
 
+					// If the evaluation of the board after making the move
+					// is better than the score of the bestMove seen so far,
+					// update the bestMove.
 					if (responseValue > bestMove.score_)
 					{
 						bestMove.score_ = responseValue;
@@ -102,16 +128,33 @@ ComputerPlayer::AIMove ComputerPlayer::findCompMove(Board& board)
 	return bestMove;
 }
 
+/**
+Find the best move for the human given a board.
+
+@param board The board to find the best move for.
+@return The best move (a structure containing the row, column, and 
+evaluation of the move). The evaluation determines who will win
+if the other player plays optimally or may be a draw.
+*/
 ComputerPlayer::AIMove ComputerPlayer::findHumanMove(Board& board)
 {
+	// Variable for the bestMove the computer can make given the
+	// board.
 	AIMove bestMove;
 
+	// Variable for the evaluation of the human player assuming
+	// he plays optimally.
 	Evaluation responseValue;
 
+	// If the game is a draw, the score (evaluation) of the move is
+	// a draw.
 	if (board.gameState() == Board::DRAW)
 	{
 		bestMove.score_ = ComputerPlayer::DRAW;
 	}
+	// Else, if the computer player can make a move to win the game,
+	//  - set the bestMove row and column to that move (happens inside
+	//    immediateWin()) and set the score to COMP_WIN
 	else if (immediateWin(board, humanPiece_, bestMove))
 	/*
 	else if ((board.gameState() == Board::XWINS && humanPiece_ == Board::X) ||
@@ -120,31 +163,37 @@ ComputerPlayer::AIMove ComputerPlayer::findHumanMove(Board& board)
 	{
 		bestMove.score_ = COMP_LOSS;
 	}
+	// Else, the board has not yet reached a terminal state so
+	// figure out the bestMove by looking it up in the table or 
+	// using minimax.
 	else
 	{
+		// Set the score to assume the worst (COMP_LOSS)
 		bestMove.score_ = COMP_WIN;
+		// Set the row and column to the first
 		bestMove.row_ = 0;
 		bestMove.column_ = 0;
+
+		// Check every square on the board
 
 		for (size_t row = 0; row < Board::MAX_ROWS; ++row)
 		{
 			for (size_t column = 0; column < Board::MAX_COLUMNS;
 				++column)
 			{
+				// If the square is empty, play there and see what the
+				// evaluation of the board is.
 				if (board.checkPiece(row, column) == Board::NONE)
 				{
 					board.playPiece(row, column, humanPiece_);
-					/*
-					 - If board is in table
-					   - return the score
-					 - Else
-					   - evaluate the score using minimax recursively
-					   - store it in the table.
-					*/
+
+					// If the board is in the table, return the score
 					if (boardToEvaluation_.in(board))
 					{
 						responseValue = boardToEvaluation_[board];
 					}
+					// Else, evaluate the score using minimax recurisvely
+					// and then store it in the table.
 					else
 					{
 						responseValue = findCompMove(board).score_;
@@ -152,6 +201,9 @@ ComputerPlayer::AIMove ComputerPlayer::findHumanMove(Board& board)
 					}
 					board.unplacePiece(row, column);
 
+					// If the evaluation of the board after making the move
+					// is better than the score of the bestMove seen so far,
+					// update the bestMove.
 					if (responseValue < bestMove.score_)
 					{
 						bestMove.score_ = responseValue;
@@ -165,60 +217,6 @@ ComputerPlayer::AIMove ComputerPlayer::findHumanMove(Board& board)
 	}
 
 	return bestMove;
-}
-
-/** 
-Get the best move for the given player for the given board.
-
-@param board The board to play
-@param player The player whose turn it is.
-*/
-ComputerPlayer::AIMove ComputerPlayer::getBestMove(
-	Board& board, Board::Piece player)
-{
-	// Get the state of the board (incomplete, someone won, or draw)
-	Board::State state = board.gameState();
-
-	// Store the best move for the player.
-	AIMove bestMove;
-
-	// Base Cases: Someone won or there is a draw.
-
-	/*
-	// If the computer won
-	if ((state == Board::XWINS && compPiece_ == Board::X) ||
-		(state == Board::OWINS && compPiece_ == Board::O))
-	{
-		bestMove = AIMove(ComputerPlayer::COMP_WIN);
-	}
-	// Else, if the computer lost
-	else if ((state == Board::XWINS && humanPiece_ == Board::X) ||
-		(state == Board::OWINS && humanPiece_ == Board::O))
-	{
-		bestMove = AIMove(ComputerPlayer::COMP_LOSS);
-	}
-	*/
-	/*
-	if (immediateWin(board, player))
-	{
-		bestMove = (player == compPiece_) ? COMP_WIN : COMP_LOSS;
-	}
-	// Else, if draw
-	else if (state == Board::DRAW)
-	{
-		bestMove = AIMove(ComputerPlayer::DRAW);
-	}
-
-	// Recursive case: Board incomplete
-	// Else, the board is incomplete so recurse.
-	else
-	{
-		bestMove = getBestMoveAux(board, player);
-
-	}
-	return bestMove;
-	*/
-
 }
 /**
 Determine if the player can make a move to win the game on the
@@ -264,55 +262,25 @@ bool ComputerPlayer::immediateWin(Board& board, Board::Piece player,
 	return retVal;
 }
 
+
 /**
-[jrm] recursive function takes in a single board and returns back
-its value.
+Functor which returns the hash code of the board.
 
-Get the moves and their scores for the player on the given board.
-
-@param board The board to get the moves for.
-@param player The player to get the moves for.
-@param moves The vector of moves.
+@param The board to hash.
+@return The hash code of the board.
 */
-void ComputerPlayer::getMoves(
-	Board& board, Board::Piece player, std::vector<AIMove>& moves)
-{
-	for (int row = 0; row < Board::MAX_ROWS; ++row)
-	{
-		for (int column = 0; column < Board::MAX_COLUMNS;
-			++column)
-		{
-			if (board.checkPiece(row, column) ==
-				Board::NONE)
-			{
-				AIMove move;
-				move.row_ = row;
-				move.column_ = column;
-
-				board.playPiece(row, column, player);
-
-				if (player == compPiece_)
-				{
-					move.score_ = getBestMove(board, humanPiece_).score_;
-				}
-				else
-				{
-					move.score_ = getBestMove(board, compPiece_).score_;
-				}
-				
-
-				moves.push_back(move);
-				board.unplacePiece(row, column);
-			}
-		}
-	}
-
-}
-
 size_t ComputerPlayer::BoardHash::operator()(const Board& board) const
 {
 	return board.hash();
 }
+
+/**
+Functor which determines wheather two boards are equal.
+
+@param lhs The board on the left hand side.
+@param rhs The board on the right hand side.
+@return True if the boards are equalivant. 
+*/
 bool ComputerPlayer::BoardEquator::operator()(const Board& lhs,
 	const Board& rhs) const
 {
